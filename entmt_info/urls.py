551912f601b660a0489918e8326645381d1fc6c0
hj0223 <hjst0223@gmail.com>
2022-03-08 11:36:24 +0900

    Register only one comment per member, Delete only one's own comment

--- entmt_info/urls.py
+++ entmt_info/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('detail/', views.e_detail, name='e_detail'),
     # path('detail_jy/', views.e_detail_jy, name='e_detail_jy'),  # 영화 상세내용 업로드
     path('results/', views.e_results, name='e_results'),
-    path('submit_comment/<int:media_id>/<media_type>', views.submit_comment, name='submit_comment'),
+    path('submit_comment/<int:media_id>/<media_type>/', views.submit_comment, name='submit_comment'),
+    path('delete_comment/<int:comment_id>/<media_type>/', views.delete_comment, name='delete_comment'),
+    path('update_comment/<int:comment_id>/<media_type>/', views.update_comment, name='update_comment'),
 ]
