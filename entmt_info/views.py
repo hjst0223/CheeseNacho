45f034fe8db84a7f8ba55712decc4e95ee054263
Yub Hahm <yub.hahm@gmail.com>
2022-03-11 22:09:07 +0900

    details page add info add titles

--- entmt_info/views.py
+++ entmt_info/views.py
@@ -263,6 +263,7 @@ def e_detail(request):
         print('movie, tv 이외의 거라서 구현이 안되어있어요!')
         print(media_type)
 
+    print(result)
     content = {
         'results': result,
         'comments': comments,
